c988834eedee5d0feb25d3e9f13c96d903130e9a
JanadaSroor <jokarasheks@gmail.com>
2026-05-27 07:45:42 +0300

    fix(jit): add explicit return type to RTDyldObjectLinkingLayer memory manager lambda for MSVC template resolution

--- src/jit/flux_jit.cpp
+++ src/jit/flux_jit.cpp
@@ -348,7 +348,9 @@ FluxJIT::FluxJIT(OptimizationLevel optLevel) : m_dataLayout(""), m_optLevel(optL
                    .setJITTargetMachineBuilder(std::move(*JTMB))
                    .setObjectLinkingLayerCreator([&](llvm::orc::ExecutionSession& ES) {
                        auto Layer = std::make_unique<llvm::orc::RTDyldObjectLinkingLayer>(
-                           ES, []() { return std::make_unique<llvm::SectionMemoryManager>(); });
+                           ES, []() -> std::unique_ptr<llvm::RuntimeDyld::MemoryManager> {
+                               return std::make_unique<llvm::SectionMemoryManager>();
+                           });
                        return Layer;
                    })
                    .create();
